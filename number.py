ada96534c49c54f697c5c57a426de567d4c9e673
Kate <ktvlphoto@outlook.com>
2022-11-17 14:24:38 +0300

    игра угадай число

---
+++ number.py
@@ -0,0 +1,10 @@
+number=43
+value= int (input ('введите число от 1 до 100: '))
+
+if value == number:
+    print ('вы угадали!')
+else:
+    if value > number:
+        print ('нужно меньше')
+    else:
+        print ('нужно больше')   
