5d521a61f315491737129953ab96d09435e1d99f
Kate <ktvlphoto@outlook.com>
2022-11-21 20:18:23 +0300

    вывод строк

---
+++ print.py
@@ -0,0 +1,13 @@
+a = 1
+b = 2
+c = 3.2
+
+# вывод строки
+print(c)
+print('*******')
+print(a, '-', b, '-', c)
+print('*******')
+print('{0} - {1} - {2}'.format(a, b, c))
+print('*******')
+print(f'{a} - {b} - {c}')
+print('*******')
