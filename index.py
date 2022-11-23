6fac216030d15707a9e3fc08c240c7db2f0a643b
Kate <ktvlphoto@outlook.com>
2022-11-23 11:55:39 +0300

    indexes

---
+++ index.py
@@ -0,0 +1,8 @@
+friend = 'Maxim'
+
+first_letter = friend[0]
+print('первая буква имени моего друга', first_letter)
+print('вторая буква имени моего друга', friend[1])
+print('третья буква имени моего друга', friend[2])
+print('четвертая буква имени моего друга', friend[-2])
+print('пятая буква имени моего друга', friend[-1])
